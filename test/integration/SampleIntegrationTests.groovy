30e1e777a5d37aa081f21af0e33723cafa662d44
Ronald Holshausen <uglyog@gmail.com>
2012-09-13 19:58:59 +1000

    added sample integration test

---
+++ test/integration/SampleIntegrationTests.groovy
@@ -0,0 +1,20 @@
+
+import static org.junit.Assert.*
+import org.junit.*
+
+class SampleIntegrationTests {
+
+    @Before
+    void setUp() {
+        // Setup logic here
+    }
+
+    @After
+    void tearDown() {
+        // Tear down logic here
+    }
+
+    @Test
+    void testSomething() {
+    }
+}
