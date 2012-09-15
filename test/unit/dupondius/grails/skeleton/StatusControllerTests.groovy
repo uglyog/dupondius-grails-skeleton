532214fb543dd1d46a6cbeaada07bca810a6f4d9
Ronald Holshausen <uglyog@gmail.com>
2012-09-15 12:03:08 +1000

    fleshed out the sample controller test

--- test/unit/dupondius/grails/skeleton/StatusControllerTests.groovy
+++ test/unit/dupondius/grails/skeleton/StatusControllerTests.groovy
@@ -1,14 +1,19 @@
 package dupondius.grails.skeleton
 
-
-
 import grails.test.mixin.*
 import org.junit.*
+import org.codehaus.groovy.grails.commons.ApplicationHolder
 
 @TestFor(StatusController)
 class StatusControllerTests {
 
-    void testSomething() {
-      
+    void testIndex() {
+      controller.index()
+	  assert controller.response.contentAsString == 'OK'
+    }
+
+    void testVersion() {
+      controller.version()
+	  assert controller.response.contentAsString == ApplicationHolder.application.metadata['app.version'].toString()
     }
 }
